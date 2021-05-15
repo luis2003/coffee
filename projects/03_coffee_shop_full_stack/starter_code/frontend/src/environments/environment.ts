a3ea3d36c47568400c1239f8c692f9eb14ceff30
luis2003 <luis2003@gmail.com>
2021-05-15 10:18:01 -0300

    fixed frontend. updated JWTs for submission

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-/* @TODO replace with your variables
+/* @TODO (DONE) replace with your variables
  * ensure all variables on this page match your project
  */
 
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'dev-cdaq-due.us.auth0.com', // the auth0 domain prefix
+    url: 'dev-cdaq-due.us', // the auth0 domain prefix
     audience: 'http://localhost:3000', // the audience set for the auth0 app
     clientId: 'RpfKBDXhHIo3QYRnKvj7FiFfaqGGn1Tw', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
